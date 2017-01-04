2ee96bd7ce0afc4233ba4ee572fb73511a36cf9e
zxc0328 <120797887@qq.com>
2017-01-04 22:56:33 +0800

    updat 2-33

---
+++ 2-33.scm
@@ -0,0 +1,9 @@
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+    initial
+    (op (car sequence)
+        (accumulate op initial (cdr sequence)))))
+
+(define (map p sequence)
+  (accumulate (lambda (x y) (cons (p x) y)) () sequence))
+
